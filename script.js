37be586813c50371db3d8a84f5c95f4e42858f35
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-01-01 10:50:05 +0600

    slick slider

---
+++ script.js
@@ -0,0 +1,26 @@
+$('.bangla-slider').slick({
+    centerMode: true,
+    arrows: true,
+    centerPadding: '60px',
+    slidesToShow: 6,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
